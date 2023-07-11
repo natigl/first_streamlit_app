b664b2d54716b4b8cdd57ed8aa4350731f627772
Natalia García Lago <115791564+natigl@users.noreply.github.com>
2023-07-11 08:59:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit 
 streamlit.title('My parents New Healthy Diner')
-🥗 🐔 🥑🍞
+
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
